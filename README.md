46b05a099bc1c0dbee16ce611fa5c90c45ca13fe
Raul <47368172+raultapia@users.noreply.github.com>
2021-02-11 15:20:55 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# VN ROS Integration
+[![Latest Version](https://img.shields.io/github/release/grvcPerception/vn_ros_integration)](https://github.com/grvcPerception/vn_ros_integration/releases)
+[![License       ](https://img.shields.io/github/license/grvcPerception/vn_ros_integration)](LICENSE)
+[![Size          ](https://img.shields.io/github/repo-size/grvcPerception/vn_ros_integration)](README.md)
+
+[Raul Tapia](https://github.com/raultapia) @ [GRVC Robotics Laboratory](https://grvc.us.es/), University of Seville, 2020.
+
+[VN-200](https://www.vectornav.com/products/vn-200) ROS integration.
+
+## Topics
+| Name         | Description                                              | Message type                  |
+|--------------|----------------------------------------------------------|-------------------------------|
+| /vn/imu      | IMU (orientation, angular velocity, linear acceleration) | sensor_msgs::Imu              |
+| /vn/mag      | Magnetometer                                             | sensor_msgs::MagneticField    |
+| /vn/gps_time | GPS time reference                                       | sensor_msgs::TimeReference    |
+| /vn/pos_lla  | GPS longitude, latitude and altitude                     | sensor_msgs::NavSatFix        |
+| /vn/pos_ecef | GPS ECEF position                                        | geometry_msgs::Vector3Stamped |
+| /vn/vel_body | Body frame velocity                                      | geometry_msgs::Vector3Stamped |
+| /vn/acc_body | Body frame acceleration                                  | geometry_msgs::Vector3Stamped |
---
+++ include/vn_ros_integration/VNdata.h
@@ -0,0 +1,70 @@
+/******************************************************************************/
+/*****                              VNdata.h                              *****/
+/*****                             Raul Tapia                             *****/
+/*****                          GRIFFIN Project                           *****/
+/*****                         GRVC-Robotics Lab.                         *****/
+/******************************************************************************/
+
+/**
+ * @file    VNdata.h
+ * @author  Raul Tapia
+ * @brief   Header file for VNdata.c
+ */
+
+#include "UARTcom.h"
+
+#define DEFAULT_UART_PORT "/dev/ttyS4"
+#define HEADER_LENGTH     10
+#define MSG_LENGTH        (140 - HEADER_LENGTH)
+#define PI                3.14159265358979323846
+
+typedef struct {
+        float x;
+        float y;
+        float z;
+} Point3F;
+
+typedef struct {
+        double x;
+        double y;
+        double z;
+} Point3D;
+
+typedef struct {
+        float roll;
+        float pitch;
+        float yaw;
+} RPY;
+
+typedef struct {
+        double latitude;
+        double longitude;
+        double altitude;
+} LLA;
+
+typedef struct {
+        double gpstime;
+
+        RPY attitude;
+        Point3F gyro;
+        Point3F accel;
+        Point3F mag;
+
+        LLA poslla;
+        Point3D posecef;
+        Point3F velbody;
+        Point3F accbody;
+} VNMeasures;
+
+void init_vn(const char*);
+void get_vn_data(VNMeasures*);
+
+// ASCII OFF ---> $VNWRG,6,0,1*B1B4
+const unsigned char write_ascii_off[]  = {0x24, 0x56, 0x4E, 0x57, 0x52, 0x47, 0x2C, 0x36, 0x2C, 0x30, 0x2C, 0x31, 0x2A, 0x42, 0x31, 0x42, 0x34, 0x0D, 0x0A};
+
+// BINARY OFF ---> $VNWRG,75,1,7,0*63E9
+const unsigned char write_binary_off[] = {0x24, 0x56, 0x4E, 0x57, 0x52, 0x47, 0x2C, 0x37, 0x35, 0x2C, 0x31, 0x2C, 0x37, 0x2C, 0x30, 0x2A, 0x36, 0x33, 0x45, 0x39, 0x0D, 0x0A};
+
+// CONFIGURATION ---> $VNWRG,75,1,10,36,0002,0700,0042,000E*FFD7
+const unsigned char write_vn_config[]  = {0x24, 0x56, 0x4E, 0x57, 0x52, 0x47, 0x2C, 0x37, 0x35, 0x2C, 0x31, 0x2C, 0x31, 0x30, 0x2C, 0x33, 0x36, 0x2C, 0x30, 0x30, 0x30, 0x32, 0x2C, 0x30, 0x37, 0x30, 0x30, 0x2C, 0x30, 0x30, 0x34, 0x32, 0x2C, 0x30, 0x30, 0x30, 0x45, 0x2A, 0x46, 0x46, 0x44, 0x37, 0x0D, 0x0A};
+const unsigned char measures_header[]  = {0xFA, 0x36, 0x02, 0x00, 0x00, 0x07, 0x42, 0x00, 0x0E, 0x00};
